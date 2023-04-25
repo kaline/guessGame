ae1b37a9d352124a989bda7f1ed8247a8c3bbe83
kaline <mesquitabfkaline@gmail.com>
2023-04-25 18:08:35 -0300

    Create guess.py

---
+++ guess.py
@@ -0,0 +1,24 @@
+
+def jogar():
+    print("####")
+    
+    secret_word = "banana"
+    enforcou = False
+    acertou = False
+    
+    index = 0
+    while(not enforcou and not acertou):
+        chute = input("Qual letra? ")
+        
+        for letra in secret_word:
+            if(chute == letra):
+                print("Encontrou {} na posicao {}".format(chute, index))
+            index = index + 1
+        
+        print("jogando")
+        
+    prin("Fim")
+        
+
+if(__name__ == "__main__"):
+    jogar()
